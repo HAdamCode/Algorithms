99f43780ba6caceb59c23c6d77aedda5feaa78fd
hunteradam <hadam@mines.edu>
2023-02-27 12:35:28 -0700

    Add counter to AlgoBowl print

--- AlgoBowl/AlgoBowl.py
+++ AlgoBowl/AlgoBowl.py
@@ -7,6 +7,7 @@ targets = [int(n) for n in input_file.readline().split(" ")]
 
 prev_used = {1}
 counter = 0
+finalArr = []
 
 def anything(cur_num): 
     greatest = 0
@@ -14,14 +15,17 @@ def anything(cur_num):
         if j <= cur_num and j > greatest:
             greatest = j
     if cur_num - greatest in prev_used:
-        print(greatest, cur_num - greatest)
+        finalArr.append([greatest, cur_num - greatest])
         prev_used.add(cur_num)
         return
     else:
         anything(cur_num-greatest)
-        print(greatest, cur_num - greatest)
+        finalArr.append([greatest, cur_num - greatest])
         prev_used.add(cur_num)
 
 for i in range(num_targets):
     cur_num = targets[i]
     anything(cur_num)
+print(len(finalArr))
+for item in finalArr:
+    print(item[0], item[1])
