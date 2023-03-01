08ccf91781569ca4f758ba6b423a343704fd7896
hunteradam <hadam@mines.edu>
2023-03-01 16:56:24 -0700

    Refactored algo bowl

--- AlgoBowl/AlgoBowl.py
+++ AlgoBowl/AlgoBowl.py
@@ -5,47 +5,48 @@ num_targets = int(input_file.readline())
 targets = [int(n) for n in input_file.readline().split(" ")]
 
 
-prev_usedWhile = [1]
-counter = 0
-finalArrWhile = []
+prevUsed = [1]
+finalArr = []
 
-def anything(cur_num): 
+def recurseFunction(cur_num): 
     greatest = 0
-    for j in prev_usedWhile:
+    for j in prevUsed:
         if j <= cur_num and j > greatest:
             greatest = j
-    if cur_num - greatest in prev_usedWhile:
-        finalArrWhile.append([greatest, cur_num - greatest])
-        prev_usedWhile.append(cur_num)
+    if cur_num - greatest in prevUsed:
+        finalArr.append([greatest, cur_num - greatest])
+        prevUsed.append(cur_num)
         return
     else:
-        anything(cur_num-greatest)
-        finalArrWhile.append([greatest, cur_num - greatest])
-        prev_usedWhile.append(cur_num)
+        recurseFunction(cur_num-greatest)
+        finalArr.append([greatest, cur_num - greatest])
+        prevUsed.append(cur_num)
 
 def whileLoop(cur_num):
     while(True):
-        if (cur_num in prev_usedWhile):
+        if (cur_num in prevUsed):
             return
-        for item in reversed(prev_usedWhile):
-            if (item + max(prev_usedWhile) <= cur_num):
-                finalArrWhile.append([item, max(prev_usedWhile)])
-                prev_usedWhile.append(item + max(prev_usedWhile))
-                # prev_usedWhile.sort(reverse=True)
+        for item in reversed(prevUsed):
+            if (item + max(prevUsed) <= cur_num):
+                finalArr.append([item, max(prevUsed)])
+                prevUsed.append(item + max(prevUsed))
                 break
 
         
 for i in range(num_targets):
     try:
-        if (max(prev_usedWhile) < targets[i]/10):
+        if (max(prevUsed) < targets[i]/10):
+            whileLoop(targets[i])
+        elif (max(prevUsed) == 1):
             whileLoop(targets[i])
         else:
-            anything(targets[i])
+            recurseFunction(targets[i])
     except:
         whileLoop(targets[i])
 
+
 f = open("myfile.txt", "w")
-print(len(finalArrWhile))
-for item in finalArrWhile:
-    temp = str(item[0]) + ' ' + str(item[1]) + '\n'
+f.write(str(len(finalArr)) + '\n')
+for final in finalArr:
+    temp = str(final[0]) + ' ' + str(final[1]) + '\n'
     f.write(temp)
