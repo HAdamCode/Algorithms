538a3d943783c4eb4f2e1668fae67faa277bcb94
hunteradam <hadam@mines.edu>
2023-03-01 12:53:09 -0700

    Double the algo double the powo

--- AlgoBowl/AlgoBowl.py
+++ AlgoBowl/AlgoBowl.py
@@ -5,39 +5,48 @@ num_targets = int(input_file.readline())
 targets = [int(n) for n in input_file.readline().split(" ")]
 
 
-prev_used = [1]
+prev_usedWhile = [1]
 counter = 0
-finalArr = []
+finalArrWhile = []
+prev_usedRecurse = {1}
+finalArrRecurse = []
 
-# def anything(cur_num): 
-#     greatest = 0
-#     for j in prev_used:
-#         if j <= cur_num and j > greatest:
-#             greatest = j
-#     if cur_num - greatest in prev_used:
-#         finalArr.append([greatest, cur_num - greatest])
-#         prev_used.add(cur_num)
-#         return
-#     else:
-#         anything(cur_num-greatest)
-#         finalArr.append([greatest, cur_num - greatest])
-#         prev_used.add(cur_num)
+def anything(cur_num): 
+    greatest = 0
+    for j in prev_usedRecurse:
+        if j <= cur_num and j > greatest:
+            greatest = j
+    if cur_num - greatest in prev_usedRecurse:
+        finalArrRecurse.append([greatest, cur_num - greatest])
+        prev_usedRecurse.add(cur_num)
+        return
+    else:
+        anything(cur_num-greatest)
+        finalArrRecurse.append([greatest, cur_num - greatest])
+        prev_usedRecurse.add(cur_num)
 
 def whileLoop(cur_num):
     while(True):
-        if (cur_num in prev_used):
+        if (cur_num in prev_usedWhile):
             return
-        for item in reversed(prev_used):
-            if (item + max(prev_used) <= cur_num):
-                finalArr.append([item, max(prev_used)])
-                prev_used.append(item + max(prev_used))
+        for item in reversed(prev_usedWhile):
+            if (item + max(prev_usedWhile) <= cur_num):
+                finalArrWhile.append([item, max(prev_usedWhile)])
+                prev_usedWhile.append(item + max(prev_usedWhile))
                 break
 
         
 
 for i in range(num_targets):
     cur_num = targets[i]
+    anything(cur_num)
     whileLoop(cur_num)
-print(len(finalArr))
-for item in finalArr:
-    print(item[0], item[1])
+
+if (len(finalArrWhile) < len(finalArrRecurse)):
+    print(len(finalArrWhile))
+    for item in finalArrWhile:
+        print(item[0], item[1])
+else:
+    print(len(finalArrRecurse))
+    for item in finalArrRecurse:
+        print(item[0], item[1])
