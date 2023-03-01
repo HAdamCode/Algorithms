e2e13ce74af87828b1b938a526c57b8d12b2ecd7
hunteradam <hadam@mines.edu>
2023-03-01 16:01:31 -0700

    Beter and better

--- AlgoBowl/AlgoBowl.py
+++ AlgoBowl/AlgoBowl.py
@@ -8,22 +8,20 @@ targets = [int(n) for n in input_file.readline().split(" ")]
 prev_usedWhile = [1]
 counter = 0
 finalArrWhile = []
-prev_usedRecurse = {1}
-finalArrRecurse = []
 
 def anything(cur_num): 
     greatest = 0
-    for j in prev_usedRecurse:
+    for j in prev_usedWhile:
         if j <= cur_num and j > greatest:
             greatest = j
-    if cur_num - greatest in prev_usedRecurse:
-        finalArrRecurse.append([greatest, cur_num - greatest])
-        prev_usedRecurse.add(cur_num)
+    if cur_num - greatest in prev_usedWhile:
+        finalArrWhile.append([greatest, cur_num - greatest])
+        prev_usedWhile.append(cur_num)
         return
     else:
         anything(cur_num-greatest)
-        finalArrRecurse.append([greatest, cur_num - greatest])
-        prev_usedRecurse.add(cur_num)
+        finalArrWhile.append([greatest, cur_num - greatest])
+        prev_usedWhile.append(cur_num)
 
 def whileLoop(cur_num):
     while(True):
@@ -33,20 +31,21 @@ def whileLoop(cur_num):
             if (item + max(prev_usedWhile) <= cur_num):
                 finalArrWhile.append([item, max(prev_usedWhile)])
                 prev_usedWhile.append(item + max(prev_usedWhile))
+                # prev_usedWhile.sort(reverse=True)
                 break
 
         
-
 for i in range(num_targets):
-    cur_num = targets[i]
-    anything(cur_num)
-    whileLoop(cur_num)
+    try:
+        if (max(prev_usedWhile) < targets[i]/10):
+            whileLoop(targets[i])
+        else:
+            anything(targets[i])
+    except:
+        whileLoop(targets[i])
 
-if (len(finalArrWhile) < len(finalArrRecurse)):
-    print(len(finalArrWhile))
-    for item in finalArrWhile:
-        print(item[0], item[1])
-else:
-    print(len(finalArrRecurse))
-    for item in finalArrRecurse:
-        print(item[0], item[1])
+f = open("myfile.txt", "w")
+print(len(finalArrWhile))
+for item in finalArrWhile:
+    temp = str(item[0]) + ' ' + str(item[1]) + '\n'
+    f.write(temp)
